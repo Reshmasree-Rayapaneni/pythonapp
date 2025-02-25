2498a550243de9f3db5c026ed9249e5712eafeb4
Reshmasree-Rayapaneni <reshmasree.r@dhanmitra.app>
2025-02-25 12:41:27 +0530

    Resolved merge conflict in app.py

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 # app.py
-<<<<<<< HEAD
+
 def hello_world():
     return "Hello, Docker!"
 
 if __name__ == "__main__":
     print(hello_world())
-=======
 
 def main():
     print("Hello from Python app in GitHub Actions!")
 
 if __name__ == "__main__":
     main()
->>>>>>> master
+
